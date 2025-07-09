4ec6382fd976a069f1b98359f9a348d7768e9ed0
hobin-han <hobin1019@gmail.com>
2025-07-09 10:26:51 +0900

    feat(RetroVideoPlaybackController): create new file

---
+++ SwiftAVPlayer/Sources/Views/RetroVideoPlaybackController/RetroVideoPlaybackController.swift
@@ -0,0 +1,16 @@
+//
+//  RetroVideoPlaybackController.swift
+//  SwiftAVPlayer
+//
+//  Created by bamiboo.han on 7/8/25.
+//
+
+import UIKit
+
+class RetroVideoPlaybackController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+}
