8fd994039ab9215847aaee238562d9b721293706
hobin-han <hobin1019@gmail.com>
2025-07-11 14:48:01 +0900

    refactor(PlayerItemStatusObserver): add functions and comments.

--- SwiftAVPlayer/Sources/Player/PlayerItemStatusObserver.swift
+++ SwiftAVPlayer/Sources/Player/PlayerItemStatusObserver.swift
@@ -16,15 +16,32 @@ class PlayerItemStatusObserver: NSObject {
     
     weak var playerItem: AVPlayerItem? {
         willSet {
-            cancellable?.cancel()
-            cancellable = newValue?.publisher(for: \.status, options: [.initial, .new])
-                .sink(receiveValue: { [weak self] status in
-                    self?.callback?(status)
-                })
+            removeObserver()
+            addObserver(newValue)
         }
     }
     
     deinit {
+        removeObserver()
+    }
+    
+    /*
+     When I try to use KVO Warning occurs like below.
+     "Block Based KVO Violation: Prefer the new block based KVO API with keypaths when using Swift 3.2 or later (block_based_kvo)"
+     
+     😢 bugs in Swift - enum property will not work with KVO
+     https://github.com/swiftlang/swift-corelibs-foundat
+     
+     So, I used Combine.
+     */
+    private func addObserver(_ playerItem: AVPlayerItem?) {
+        cancellable = playerItem?.publisher(for: \.status, options: [.initial, .new])
+            .sink(receiveValue: { [weak self] status in
+                self?.callback?(status)
+            })
+    }
+    
+    private func removeObserver() {
         cancellable?.cancel()
     }
 }
