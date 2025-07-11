21320899dd67a3c1f459b589dc98f06e82ef37be
hobin-han <hobin1019@gmail.com>
2025-07-11 14:38:54 +0900

    refactor(PlayerItemStatusObserver): remove warning - use Combine instead of KVO

--- SwiftAVPlayer/Sources/Player/PlayerItemStatusObserver.swift
+++ SwiftAVPlayer/Sources/Player/PlayerItemStatusObserver.swift
@@ -6,36 +6,25 @@
 //
 
 import AVFoundation
+import Combine
 
 class PlayerItemStatusObserver: NSObject {
     
     var callback: ((AVPlayerItem.Status) -> Void)?
     
+    private var cancellable: AnyCancellable?
+    
     weak var playerItem: AVPlayerItem? {
         willSet {
-            playerItem?.removeObserver(self, forKeyPath: #keyPath(AVPlayerItem.status))
-            newValue?.addObserver(self, forKeyPath: #keyPath(AVPlayerItem.status), options: [.initial, .new], context: nil)
+            cancellable?.cancel()
+            cancellable = newValue?.publisher(for: \.status, options: [.initial, .new])
+                .sink(receiveValue: { [weak self] status in
+                    self?.callback?(status)
+                })
         }
     }
     
     deinit {
-        playerItem?.removeObserver(self, forKeyPath: #keyPath(AVPlayerItem.status))
-    }
-    
-    /*
-     [Warning]
-     "Block Based KVO Violation: Prefer the new block based KVO API with keypaths when using Swift 3.2 or later (block_based_kvo)"
-     
-     bugs in Swift - enum property will not work with KVO
-     https://github.com/swiftlang/swift-corelibs-foundat
-     */
-    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey: Any]?, context: UnsafeMutableRawPointer?) {
-        switch keyPath {
-        case #keyPath(AVPlayerItem.status):
-            if let number = change?[.newKey] as? NSNumber, let status = AVPlayerItem.Status(rawValue: number.intValue) {
-                callback?(status)
-            }
-        default: break
-        }
+        cancellable?.cancel()
     }
 }
