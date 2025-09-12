f4892c86ff037b86923896c2ffeaf4e5983b42e7
hobin-han <hobin1019@gmail.com>
2025-09-12 11:53:31 +0900

    refactor(remove): remove unused init functions

--- SwiftAVPlayer/Sources/Views/VideoDetailViewController/VideoDetailViewController.swift
+++ SwiftAVPlayer/Sources/Views/VideoDetailViewController/VideoDetailViewController.swift
@@ -41,14 +41,6 @@ final class VideoDetailViewController: UIViewController {
         }
     }
     
-    init() {
-        super.init(nibName: nil, bundle: nil)
-    }
-    
-    required init?(coder: NSCoder) {
-        super.init(coder: coder)
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
